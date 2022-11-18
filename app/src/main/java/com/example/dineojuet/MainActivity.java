b21b43c3497cb9ca03dace8b73654cab15ac6cc6
Gautam Singh <singh.gautam2508@gmail.com>
2022-11-18 20:21:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dineojuet/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dineojuet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public classMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
