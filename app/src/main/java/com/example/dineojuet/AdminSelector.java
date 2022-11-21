15a158a0283e09b64ff00e8423510c6af6a9383a
Gautam Singh <singh.gautam2508@gmail.com>
2022-11-21 22:28:48 +0530

    Student ID selector on click.

--- app/src/main/java/com/example/dineojuet/AdminSelector.java
+++ app/src/main/java/com/example/dineojuet/AdminSelector.java
@@ -1,7 +1,10 @@
 package com.example.dineojuet;
 
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminSelector extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class AdminSelector extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_selector);
+
+        final Button button = (Button) findViewById(R.id.Student);
+
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+
+            }
+        });
     }
 }
