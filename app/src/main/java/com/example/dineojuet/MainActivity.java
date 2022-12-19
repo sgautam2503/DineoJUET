d90ec1d8ef091cee1c88d677e9d5af0819ff5c24
ayush960 <ayush1111shivhare@gmail.com>
2022-12-19 21:30:44 +0530

    verification link email, adding scroll view, and some other changes

--- app/src/main/java/com/example/dineojuet/MainActivity.java
+++ app/src/main/java/com/example/dineojuet/MainActivity.java
@@ -1,23 +1,18 @@
 package com.example.dineojuet;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
-import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.dineojuet.databinding.ActivityMainBinding;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
@@ -29,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
         super.onCreate(savedInstanceState);
         binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -37,51 +32,52 @@ public class MainActivity extends AppCompatActivity {
         firebaseFirestore=FirebaseFirestore.getInstance();
         progressDialog=new ProgressDialog(this);
 
-        binding.signup.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view)
-            {
-                String firstname=binding.firstname.getText().toString().trim();
-                String lastname=binding.lastname.getText().toString().trim();
-                String email=binding.email.getText().toString().trim();
-                String enrol=binding.enrol.getText().toString().trim();
-                String password=binding.password.getText().toString();
+        binding.signup.setOnClickListener(view -> {
+            String firstname=binding.firstname.getText().toString().trim();
+            String lastname=binding.lastname.getText().toString().trim();
+            String email=binding.email.getText().toString().trim();
+            String enrol=binding.enrol.getText().toString().trim();
+            String password=binding.password.getText().toString();
 
-                progressDialog.show();
-                firebaseAuth.createUserWithEmailAndPassword(email,password)
-                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                            @Override
-                            public void onSuccess(AuthResult authResult) {
-                                startActivity(new Intent(MainActivity.this,LoginActivity.class));
-                                Toast.makeText(MainActivity.this, "Your Account has been Created.", Toast.LENGTH_SHORT).show();
-                                progressDialog.cancel();
+            progressDialog.show();
+            firebaseAuth.createUserWithEmailAndPassword(email,password)
+                    .addOnSuccessListener(authResult -> {
+                        SendVerificationEmail();
+                        if (Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).isEmailVerified()) {
+                            startActivity(new Intent(MainActivity.this, OtpVerification.class));
+                            Toast.makeText(MainActivity.this, "Your Account has been Created.", Toast.LENGTH_SHORT).show();
 
-                                firebaseFirestore.collection("User")
-                                        .document(FirebaseAuth.getInstance().getUid())
-                                        .set(new UserModel(firstname,lastname,email,enrol,password));
-                            }
-                        })
+                            firebaseFirestore.collection("User")
+                                    .document(Objects.requireNonNull(FirebaseAuth.getInstance().getUid()))
+                                    .set(new UserModel(firstname, lastname, email, enrol, password));
+                            Toast.makeText(MainActivity.this, "Your Account has been Created.", Toast.LENGTH_SHORT).show();
+                        }
+                    })
 
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                progressDialog.cancel();
-                                Toast.makeText(MainActivity.this, e.getMessage(),Toast.LENGTH_SHORT).show();
-                            }
-                        });
-            }
-                                          }
+                    .addOnFailureListener(e -> {
+                        progressDialog.cancel();
+                        Toast.makeText(MainActivity.this, e.getMessage(),Toast.LENGTH_SHORT).show();
+                    });
+        }
 
         );
 
-        binding.signininstead.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
-            }
-        });
+        binding.signininstead.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, LoginActivity.class)));
 
     }
-    
+
+    private void SendVerificationEmail() {
+        Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser())
+                .sendEmailVerification()
+                .addOnSuccessListener(unused -> {
+                    progressDialog.dismiss();
+                    Toast.makeText(MainActivity.this,"Verification link Sent to Your Email id",Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(e -> {
+                    progressDialog.dismiss();
+                   Toast.makeText(MainActivity.this,"Verification link not sent",Toast.LENGTH_SHORT).show();
+                });
+    }
+
 
 }
